e2d50e60400be8af9f9815dc308e52af3484f8ff
pdhar <pranav.dhar2@gmail.com>
2016-02-21 13:39:42 -0500

    added canvas drawing of nested circles

--- src/gameLogic_test.ts
+++ src/gameLogic_test.ts
@@ -1,39 +1,24 @@
 describe("In Carrom Game Logic", function() {
 
-  function expectMove(
-      isOk: boolean,
-      turnIndexBeforeMove: number,
-      boardBeforeMove: Board,
-      row: number,
-      col: number,
-      boardAfterMove: Board,
-      turnIndexAfterMove: number,
-      endMatchScores: number[]): void {
-    let stateTransition: IStateTransition = {
-      turnIndexBeforeMove: turnIndexBeforeMove,
-      stateBeforeMove: boardBeforeMove ? {board: boardBeforeMove, delta: null} : null,
-      move: {
-        turnIndexAfterMove: turnIndexAfterMove,
-        endMatchScores: endMatchScores,
-        stateAfterMove: {board: boardAfterMove, delta: {row: row, col: col}}
-      },
-      numberOfPlayers: null
-    };
-    if (isOk) {
-      gameLogic.checkMoveOk(stateTransition);
-    } else {
-      // We expect an exception to be thrown :)
-      let didThrowException = false;
-      try {
-        gameLogic.checkMoveOk(stateTransition);
-      } catch (e) {
-        didThrowException = true;
-      }
-      if (!didThrowException) {
-        throw new Error("We expect an illegal move, but checkMoveOk didn't throw any exception!")
-      }
-    }
+  function testSomething(a : boolean): void {
+    if(a) 
+      throw new Error("We expect an illegal move, but checkMoveOk didn't throw any exception!");
   }
-
+  
+  function testSomeValue(): number {
+    return 1;
+  }
+  
+  it("testing some bs function", function() {
+    testSomething(false);
+  });
+  
+  it("testing some bs function 2", function() {
+    testSomething(false);
+  });
+  
+  it("testing some bs function returning value", function() {
+    expect(testSomeValue()).toBe(2);
+  });
   
 });
