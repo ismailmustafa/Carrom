535850ef69bf48d8937309cfbfe51273462eabb3
Ismail Mustafa <ismailmustafa@rocketmail.com>
2016-02-21 12:20:54 -0500

    Implemented basic starting logic

--- src/gameLogic.ts
+++ src/gameLogic.ts
@@ -1,151 +1,98 @@
-type Board = string[][];
-interface BoardDelta {
-  row: number;
-  col: number;
+type Board = Coin[];
+
+interface BoardSize {
+    width: number;
+    height: number;
+    centerX: number;
+    centerY: number;
+}
+
+interface Striker {
+    xPos: number;
+    yPos: number;
+    size: number;
+}
+
+enum CoinType {
+    Queen,
+    Black,
+    White
+}
+
+interface Coin {
+    xPos: number;
+    yPos: number;
+    size: number;
+    type: CoinType;
 }
 interface IState {
-  board: Board;
-  delta: BoardDelta;
+    board: Board;
+    boardSize : BoardSize;
 }
 
 module gameLogic {
-  export const ROWS = 3;
-  export const COLS = 3;
+  // Coin counts
+  export const NUMCOINS = 19;
+  export const NUMBLACK = 9;
+  export const NUMWHITE = 9;
+  export const NUMQUEEN = 1;
+  
+  // Coin initializer properties
+  export const FIRSTLAYER = 6;
+  export const SECONDLAYER = 12;
+  export const COINSIZE = 1;
+  
+  // Board dimensions
+  export const BOARDWIDTH = 40;
+  export const BOARDHEIGHT = 40;
 
-  /** Returns the initial TicTacToe board, which is a ROWSxCOLS matrix containing ''. */
+  // Return initial board 
   function getInitialBoard(): Board {
+    let boardSize : BoardSize = getInitialSize();
     let board: Board = [];
-    for (let i = 0; i < ROWS; i++) {
-      board[i] = [];
-      for (let j = 0; j < COLS; j++) {
-        board[i][j] = '';
-      }
-    }
+    
+    // set the position of hte queen
+    // insert that qqueen into board
+    let queen : Coin = {xPos:boardSize.centerX,
+                        yPos:boardSize.centerY,
+                        size:COINSIZE,
+                        type:CoinType.Queen};
+    board.push(queen);
     return board;
+  }
+      
+    // Second ring
+    //for (let j = 0; j < COLS; j++) {
+    //    board[i][j] = '';
+    //}
+  
+    function calculateFirstLayer(queen : Coin) : Coin[] {
+        let coins : Coin[] = [];
+      // Calculate first ring of coins
+        let color : boolean = false;
+        for (let i = 0; i < FIRSTLAYER; i++) {
+            let x = Math.cos(i*((2*Math.PI)/FIRSTLAYER)) * queen.size;
+            let y = Math.sin(i*((2*Math.PI)/FIRSTLAYER)) * queen.size;
+            let coinType = color ? CoinType.Black : CoinType.White;
+            color = !color;
+            let coin : Coin = {xPos:x,
+                               yPos:y,
+                               size:COINSIZE,
+                               type:coinType};
+            coins.push(coin);      
+        }
+        return coins;
+ }
+  
+  function getInitialSize(): BoardSize {
+      let boardSize : BoardSize = {width: BOARDWIDTH, 
+                                   height: BOARDHEIGHT,
+                                   centerX: BOARDWIDTH/2,
+                                   centerY: BOARDHEIGHT/2};
+      return boardSize;
   }
 
   export function getInitialState(): IState {
-    return {board: getInitialBoard(), delta: null};
-  }
-
-  /**
-   * Returns true if the game ended in a tie because there are no empty cells.
-   * E.g., isTie returns true for the following board:
-   *     [['X', 'O', 'X'],
-   *      ['X', 'O', 'O'],
-   *      ['O', 'X', 'X']]
-   */
-  function isTie(board: Board): boolean {
-    for (let i = 0; i < ROWS; i++) {
-      for (let j = 0; j < COLS; j++) {
-        if (board[i][j] === '') {
-          // If there is an empty cell then we do not have a tie.
-          return false;
-        }
-      }
-    }
-    // No empty cells, so we have a tie!
-    return true;
-  }
-
-  /**
-   * Return the winner (either 'X' or 'O') or '' if there is no winner.
-   * The board is a matrix of size 3x3 containing either 'X', 'O', or ''.
-   * E.g., getWinner returns 'X' for the following board:
-   *     [['X', 'O', ''],
-   *      ['X', 'O', ''],
-   *      ['X', '', '']]
-   */
-  function getWinner(board: Board): string {
-    let boardString = '';
-    for (let i = 0; i < ROWS; i++) {
-      for (let j = 0; j < COLS; j++) {
-        let cell = board[i][j];
-        boardString += cell === '' ? ' ' : cell;
-      }
-    }
-    let win_patterns = [
-      'XXX......',
-      '...XXX...',
-      '......XXX',
-      'X..X..X..',
-      '.X..X..X.',
-      '..X..X..X',
-      'X...X...X',
-      '..X.X.X..'
-    ];
-    for (let win_pattern of win_patterns) {
-      let x_regexp = new RegExp(win_pattern);
-      let o_regexp = new RegExp(win_pattern.replace(/X/g, 'O'));
-      if (x_regexp.test(boardString)) {
-        return 'X';
-      }
-      if (o_regexp.test(boardString)) {
-        return 'O';
-      }
-    }
-    return '';
-  }
-
-  /**
-   * Returns the move that should be performed when player
-   * with index turnIndexBeforeMove makes a move in cell row X col.
-   */
-  export function createMove(
-      stateBeforeMove: IState, row: number, col: number, turnIndexBeforeMove: number): IMove {
-    if (!stateBeforeMove) { // stateBeforeMove is null in a new match.
-      stateBeforeMove = getInitialState();
-    }
-    let board: Board = stateBeforeMove.board;
-    if (board[row][col] !== '') {
-      throw new Error("One can only make a move in an empty position!");
-    }
-    if (getWinner(board) !== '' || isTie(board)) {
-      throw new Error("Can only make a move if the game is not over!");
-    }
-    let boardAfterMove = angular.copy(board);
-    boardAfterMove[row][col] = turnIndexBeforeMove === 0 ? 'X' : 'O';
-    let winner = getWinner(boardAfterMove);
-    let endMatchScores: number[];
-    let turnIndexAfterMove: number;
-    if (winner !== '' || isTie(boardAfterMove)) {
-      // Game over.
-      turnIndexAfterMove = -1;
-      endMatchScores = winner === 'X' ? [1, 0] : winner === 'O' ? [0, 1] : [0, 0];
-    } else {
-      // Game continues. Now it's the opponent's turn (the turn switches from 0 to 1 and 1 to 0).
-      turnIndexAfterMove = 1 - turnIndexBeforeMove;
-      endMatchScores = null;
-    }
-    let delta: BoardDelta = {row: row, col: col};
-    let stateAfterMove: IState = {delta: delta, board: boardAfterMove};
-    return {endMatchScores: endMatchScores, turnIndexAfterMove: turnIndexAfterMove, stateAfterMove: stateAfterMove};
-  }
-
-  export function checkMoveOk(stateTransition: IStateTransition): void {
-    // We can assume that turnIndexBeforeMove and stateBeforeMove are legal, and we need
-    // to verify that the move is OK.
-    let turnIndexBeforeMove = stateTransition.turnIndexBeforeMove;
-    let stateBeforeMove: IState = stateTransition.stateBeforeMove;
-    let move: IMove = stateTransition.move;
-    let deltaValue: BoardDelta = stateTransition.move.stateAfterMove.delta;
-    let row = deltaValue.row;
-    let col = deltaValue.col;
-    let expectedMove = createMove(stateBeforeMove, row, col, turnIndexBeforeMove);
-    if (!angular.equals(move, expectedMove)) {
-      throw new Error("Expected move=" + angular.toJson(expectedMove, true) +
-          ", but got stateTransition=" + angular.toJson(stateTransition, true))
-    }
-  }
-
-  export function forSimpleTestHtml() {
-    var move = gameLogic.createMove(null, 0, 0, 0);
-    log.log("move=", move);
-    var params: IStateTransition = {
-      turnIndexBeforeMove: 0,
-      stateBeforeMove: null,
-      move: move,
-      numberOfPlayers: 2};
-    gameLogic.checkMoveOk(params);
+    return {board: getInitialBoard(), boardSize: getInitialSize()};
   }
 }
