09374f4e713f7569f9fcb666cac00fcb9d3aaafb
Ismail Mustafa <ismailmustafa@rocketmail.com>
2016-02-21 12:20:54 -0500

    Implemented basic starting logic

--- ts_output_readonly_do_NOT_change_manually/src/game.js
+++ ts_output_readonly_do_NOT_change_manually/src/game.js
@@ -1,152 +1,147 @@
-;
-var game;
-(function (game) {
-    // I export all variables to make it easy to debug in the browser by
-    // simply typing in the console:
-    // game.state
-    game.animationEnded = false;
-    game.canMakeMove = false;
-    game.isComputerTurn = false;
-    game.move = null;
-    game.state = null;
-    game.isHelpModalShown = false;
-    function init() {
-        translate.setTranslations(getTranslations());
-        translate.setLanguage('en');
-        log.log("Translation of 'RULES_OF_TICTACTOE' is " + translate('RULES_OF_TICTACTOE'));
-        resizeGameAreaService.setWidthToHeight(1);
-        moveService.setGame({
-            minNumberOfPlayers: 2,
-            maxNumberOfPlayers: 2,
-            checkMoveOk: gameLogic.checkMoveOk,
-            updateUI: updateUI
-        });
-        // See http://www.sitepoint.com/css3-animation-javascript-event-handlers/
-        document.addEventListener("animationend", animationEndedCallback, false); // standard
-        document.addEventListener("webkitAnimationEnd", animationEndedCallback, false); // WebKit
-        document.addEventListener("oanimationend", animationEndedCallback, false); // Opera
-        var w = window;
-        if (w["HTMLInspector"]) {
-            setInterval(function () {
-                w["HTMLInspector"].inspect({
-                    excludeRules: ["unused-classes", "script-placement"],
-                });
-            }, 3000);
-        }
-    }
-    game.init = init;
-    function getTranslations() {
-        return {
-            RULES_OF_TICTACTOE: {
-                en: "Rules of BattleTetris",
-                iw: "חוקי המשחק",
-            },
-            RULES_SLIDE1: {
-                en: "You and your opponent take turns to mark the grid in an empty spot. The first mark is X, then O, then X, then O, etc.",
-                iw: "אתה והיריב מסמנים איקס או עיגול כל תור",
-            },
-            RULES_SLIDE2: {
-                en: "The first to mark a whole row, column or diagonal wins.",
-                iw: "הראשון שמסמן שורה, עמודה או אלכסון מנצח",
-            },
-            CLOSE: {
-                en: "Close",
-                iw: "סגור",
-            },
-        };
-    }
-    function animationEndedCallback() {
-        $rootScope.$apply(function () {
-            log.info("Animation ended");
-            game.animationEnded = true;
-            sendComputerMove();
-        });
-    }
-    function sendComputerMove() {
-        if (!game.isComputerTurn) {
-            return;
-        }
-        game.isComputerTurn = false; // to make sure the computer can only move once.
-        moveService.makeMove(aiService.findComputerMove(game.move));
-    }
-    function updateUI(params) {
-        log.info("Game got updateUI:", params);
-        game.animationEnded = false;
-        game.move = params.move;
-        game.state = game.move.stateAfterMove;
-        if (!game.state) {
-            game.state = gameLogic.getInitialState();
-        }
-        game.canMakeMove = game.move.turnIndexAfterMove >= 0 &&
-            params.yourPlayerIndex === game.move.turnIndexAfterMove; // it's my turn
-        // Is it the computer's turn?
-        game.isComputerTurn = game.canMakeMove &&
-            params.playersInfo[params.yourPlayerIndex].playerId === '';
-        if (game.isComputerTurn) {
-            // To make sure the player won't click something and send a move instead of the computer sending a move.
-            game.canMakeMove = false;
-            // We calculate the AI move only after the animation finishes,
-            // because if we call aiService now
-            // then the animation will be paused until the javascript finishes.
-            if (!game.state.delta) {
-                // This is the first move in the match, so
-                // there is not going to be an animation, so
-                // call sendComputerMove() now (can happen in ?onlyAIs mode)
-                sendComputerMove();
-            }
-        }
-    }
-    function cellClicked(row, col) {
-        log.info("Clicked on cell:", row, col);
-        if (window.location.search === '?throwException') {
-            throw new Error("Throwing the error because URL has '?throwException'");
-        }
-        if (!game.canMakeMove) {
-            return;
-        }
-        try {
-            var nextMove = gameLogic.createMove(game.state, row, col, game.move.turnIndexAfterMove);
-            game.canMakeMove = false; // to prevent making another move
-            moveService.makeMove(nextMove);
-        }
-        catch (e) {
-            log.info(["Cell is already full in position:", row, col]);
-            return;
-        }
-    }
-    game.cellClicked = cellClicked;
-    function shouldShowImage(row, col) {
-        var cell = game.state.board[row][col];
-        return cell !== "";
-    }
-    game.shouldShowImage = shouldShowImage;
-    function isPieceX(row, col) {
-        return game.state.board[row][col] === 'X';
-    }
-    game.isPieceX = isPieceX;
-    function isPieceO(row, col) {
-        return game.state.board[row][col] === 'O';
-    }
-    game.isPieceO = isPieceO;
-    function shouldSlowlyAppear(row, col) {
-        return !game.animationEnded &&
-            game.state.delta &&
-            game.state.delta.row === row && game.state.delta.col === col;
-    }
-    game.shouldSlowlyAppear = shouldSlowlyAppear;
-    function clickedOnModal(evt) {
-        if (evt.target === evt.currentTarget) {
-            evt.preventDefault();
-            evt.stopPropagation();
-            game.isHelpModalShown = false;
-        }
-        return true;
-    }
-    game.clickedOnModal = clickedOnModal;
-})(game || (game = {}));
-angular.module('myApp', ['ngTouch', 'ui.bootstrap', 'gameServices'])
-    .run(function () {
-    $rootScope['game'] = game;
-    game.init();
-});
+// interface SupportedLanguages { en: string, iw: string};
+// interface Translations {
+//   [index: string]: SupportedLanguages;
+// }
+// module game {
+//   // I export all variables to make it easy to debug in the browser by
+//   // simply typing in the console:
+//   // game.state
+//   export let animationEnded = false;
+//   export let canMakeMove = false;
+//   export let isComputerTurn = false;
+//   export let move: IMove = null;
+//   export let state: IState = null;
+//   export let isHelpModalShown: boolean = false;
+//   export function init() {
+//     translate.setTranslations(getTranslations());
+//     translate.setLanguage('en');
+//     log.log("Translation of 'RULES_OF_TICTACTOE' is " + translate('RULES_OF_TICTACTOE'));
+//     resizeGameAreaService.setWidthToHeight(1);
+//     moveService.setGame({
+//       minNumberOfPlayers: 2,
+//       maxNumberOfPlayers: 2,
+//       checkMoveOk: gameLogic.checkMoveOk,
+//       updateUI: updateUI
+//     });
+//     // See http://www.sitepoint.com/css3-animation-javascript-event-handlers/
+//     document.addEventListener("animationend", animationEndedCallback, false); // standard
+//     document.addEventListener("webkitAnimationEnd", animationEndedCallback, false); // WebKit
+//     document.addEventListener("oanimationend", animationEndedCallback, false); // Opera
+//     let w: any = window;
+//     if (w["HTMLInspector"]) {
+//       setInterval(function () {
+//         w["HTMLInspector"].inspect({
+//           excludeRules: ["unused-classes", "script-placement"],
+//         });
+//       }, 3000);
+//     }
+//   }
+//   function getTranslations(): Translations {
+//     return {
+//       RULES_OF_TICTACTOE: {
+//         en: "Rules of BattleTetris",
+//         iw: "חוקי המשחק",
+//       },
+//       RULES_SLIDE1: {
+//         en: "You and your opponent take turns to mark the grid in an empty spot. The first mark is X, then O, then X, then O, etc.",
+//         iw: "אתה והיריב מסמנים איקס או עיגול כל תור",
+//       },
+//       RULES_SLIDE2: {
+//         en: "The first to mark a whole row, column or diagonal wins.",
+//         iw: "הראשון שמסמן שורה, עמודה או אלכסון מנצח",
+//       },
+//       CLOSE:  {
+//         en: "Close",
+//         iw: "סגור",
+//       },
+//     };
+//   }
+//   function animationEndedCallback() {
+//     $rootScope.$apply(function () {
+//       log.info("Animation ended");
+//       animationEnded = true;
+//       sendComputerMove();
+//     });
+//   }
+//   function sendComputerMove() {
+//     if (!isComputerTurn) {
+//       return;
+//     }
+//     isComputerTurn = false; // to make sure the computer can only move once.
+//     moveService.makeMove(aiService.findComputerMove(move));
+//   }
+//   function updateUI(params: IUpdateUI): void {
+//     log.info("Game got updateUI:", params);
+//     animationEnded = false;
+//     move = params.move;
+//     state = move.stateAfterMove;
+//     if (!state) {
+//       state = gameLogic.getInitialState();
+//     }
+//     canMakeMove = move.turnIndexAfterMove >= 0 && // game is ongoing
+//       params.yourPlayerIndex === move.turnIndexAfterMove; // it's my turn
+//     // Is it the computer's turn?
+//     isComputerTurn = canMakeMove &&
+//         params.playersInfo[params.yourPlayerIndex].playerId === '';
+//     if (isComputerTurn) {
+//       // To make sure the player won't click something and send a move instead of the computer sending a move.
+//       canMakeMove = false;
+//       // We calculate the AI move only after the animation finishes,
+//       // because if we call aiService now
+//       // then the animation will be paused until the javascript finishes.
+//       if (!state.delta) {
+//         // This is the first move in the match, so
+//         // there is not going to be an animation, so
+//         // call sendComputerMove() now (can happen in ?onlyAIs mode)
+//         sendComputerMove();
+//       }
+//     }
+//   }
+//   export function cellClicked(row: number, col: number): void {
+//     log.info("Clicked on cell:", row, col);
+//     if (window.location.search === '?throwException') { // to test encoding a stack trace with sourcemap
+//       throw new Error("Throwing the error because URL has '?throwException'");
+//     }
+//     if (!canMakeMove) {
+//       return;
+//     }
+//     try {
+//       let nextMove = gameLogic.createMove(
+//           state, row, col, move.turnIndexAfterMove);
+//       canMakeMove = false; // to prevent making another move
+//       moveService.makeMove(nextMove);
+//     } catch (e) {
+//       log.info(["Cell is already full in position:", row, col]);
+//       return;
+//     }
+//   }
+//   export function shouldShowImage(row: number, col: number): boolean {
+//     let cell = state.board[row][col];
+//     return cell !== "";
+//   }
+//   export function isPieceX(row: number, col: number): boolean {
+//     return state.board[row][col] === 'X';
+//   }
+//   export function isPieceO(row: number, col: number): boolean {
+//     return state.board[row][col] === 'O';
+//   }
+//   export function shouldSlowlyAppear(row: number, col: number): boolean {
+//     return !animationEnded &&
+//         state.delta &&
+//         state.delta.row === row && state.delta.col === col;
+//   }
+//   export function clickedOnModal(evt: Event) {
+//     if (evt.target === evt.currentTarget) {
+//       evt.preventDefault();
+//       evt.stopPropagation();
+//       isHelpModalShown = false;
+//     }
+//     return true;
+//   }
+// }
+// angular.module('myApp', ['ngTouch', 'ui.bootstrap', 'gameServices'])
+//   .run(function () {
+//     $rootScope['game'] = game;
+//     game.init();
+//   });
 //# sourceMappingURL=game.js.map
--- ts_output_readonly_do_NOT_change_manually/src/gameLogic.js
+++ ts_output_readonly_do_NOT_change_manually/src/gameLogic.js
@@ -1,141 +1,67 @@
+var CoinType;
+(function (CoinType) {
+    CoinType[CoinType["Queen"] = 0] = "Queen";
+    CoinType[CoinType["Black"] = 1] = "Black";
+    CoinType[CoinType["White"] = 2] = "White";
+})(CoinType || (CoinType = {}));
 var gameLogic;
 (function (gameLogic) {
-    gameLogic.ROWS = 3;
-    gameLogic.COLS = 3;
-    /** Returns the initial TicTacToe board, which is a ROWSxCOLS matrix containing ''. */
+    // Coin counts
+    gameLogic.NUMCOINS = 19;
+    gameLogic.NUMBLACK = 9;
+    gameLogic.NUMWHITE = 9;
+    gameLogic.NUMQUEEN = 1;
+    // Coin initializer properties
+    gameLogic.FIRSTLAYER = 6;
+    gameLogic.SECONDLAYER = 12;
+    gameLogic.COINSIZE = 1;
+    // Board dimensions
+    gameLogic.BOARDWIDTH = 40;
+    gameLogic.BOARDHEIGHT = 40;
+    // Return initial board 
     function getInitialBoard() {
+        var boardSize = getInitialSize();
         var board = [];
-        for (var i = 0; i < gameLogic.ROWS; i++) {
-            board[i] = [];
-            for (var j = 0; j < gameLogic.COLS; j++) {
-                board[i][j] = '';
-            }
-        }
+        // set the position of hte queen
+        // insert that qqueen into board
+        var queen = { xPos: boardSize.centerX,
+            yPos: boardSize.centerY,
+            size: gameLogic.COINSIZE,
+            type: CoinType.Queen };
+        board.push(queen);
         return board;
     }
+    // Second ring
+    //for (let j = 0; j < COLS; j++) {
+    //    board[i][j] = '';
+    //}
+    function calculateFirstLayer(queen) {
+        var coins = [];
+        // Calculate first ring of coins
+        var color = false;
+        for (var i = 0; i < gameLogic.FIRSTLAYER; i++) {
+            var x = Math.cos(i * ((2 * Math.PI) / gameLogic.FIRSTLAYER)) * queen.size;
+            var y = Math.sin(i * ((2 * Math.PI) / gameLogic.FIRSTLAYER)) * queen.size;
+            var coinType = color ? CoinType.Black : CoinType.White;
+            color = !color;
+            var coin = { xPos: x,
+                yPos: y,
+                size: gameLogic.COINSIZE,
+                type: coinType };
+            coins.push(coin);
+        }
+        return coins;
+    }
+    function getInitialSize() {
+        var boardSize = { width: gameLogic.BOARDWIDTH,
+            height: gameLogic.BOARDHEIGHT,
+            centerX: gameLogic.BOARDWIDTH / 2,
+            centerY: gameLogic.BOARDHEIGHT / 2 };
+        return boardSize;
+    }
     function getInitialState() {
-        return { board: getInitialBoard(), delta: null };
+        return { board: getInitialBoard(), boardSize: getInitialSize() };
     }
     gameLogic.getInitialState = getInitialState;
-    /**
-     * Returns true if the game ended in a tie because there are no empty cells.
-     * E.g., isTie returns true for the following board:
-     *     [['X', 'O', 'X'],
-     *      ['X', 'O', 'O'],
-     *      ['O', 'X', 'X']]
-     */
-    function isTie(board) {
-        for (var i = 0; i < gameLogic.ROWS; i++) {
-            for (var j = 0; j < gameLogic.COLS; j++) {
-                if (board[i][j] === '') {
-                    // If there is an empty cell then we do not have a tie.
-                    return false;
-                }
-            }
-        }
-        // No empty cells, so we have a tie!
-        return true;
-    }
-    /**
-     * Return the winner (either 'X' or 'O') or '' if there is no winner.
-     * The board is a matrix of size 3x3 containing either 'X', 'O', or ''.
-     * E.g., getWinner returns 'X' for the following board:
-     *     [['X', 'O', ''],
-     *      ['X', 'O', ''],
-     *      ['X', '', '']]
-     */
-    function getWinner(board) {
-        var boardString = '';
-        for (var i = 0; i < gameLogic.ROWS; i++) {
-            for (var j = 0; j < gameLogic.COLS; j++) {
-                var cell = board[i][j];
-                boardString += cell === '' ? ' ' : cell;
-            }
-        }
-        var win_patterns = [
-            'XXX......',
-            '...XXX...',
-            '......XXX',
-            'X..X..X..',
-            '.X..X..X.',
-            '..X..X..X',
-            'X...X...X',
-            '..X.X.X..'
-        ];
-        for (var _i = 0; _i < win_patterns.length; _i++) {
-            var win_pattern = win_patterns[_i];
-            var x_regexp = new RegExp(win_pattern);
-            var o_regexp = new RegExp(win_pattern.replace(/X/g, 'O'));
-            if (x_regexp.test(boardString)) {
-                return 'X';
-            }
-            if (o_regexp.test(boardString)) {
-                return 'O';
-            }
-        }
-        return '';
-    }
-    /**
-     * Returns the move that should be performed when player
-     * with index turnIndexBeforeMove makes a move in cell row X col.
-     */
-    function createMove(stateBeforeMove, row, col, turnIndexBeforeMove) {
-        if (!stateBeforeMove) {
-            stateBeforeMove = getInitialState();
-        }
-        var board = stateBeforeMove.board;
-        if (board[row][col] !== '') {
-            throw new Error("One can only make a move in an empty position!");
-        }
-        if (getWinner(board) !== '' || isTie(board)) {
-            throw new Error("Can only make a move if the game is not over!");
-        }
-        var boardAfterMove = angular.copy(board);
-        boardAfterMove[row][col] = turnIndexBeforeMove === 0 ? 'X' : 'O';
-        var winner = getWinner(boardAfterMove);
-        var endMatchScores;
-        var turnIndexAfterMove;
-        if (winner !== '' || isTie(boardAfterMove)) {
-            // Game over.
-            turnIndexAfterMove = -1;
-            endMatchScores = winner === 'X' ? [1, 0] : winner === 'O' ? [0, 1] : [0, 0];
-        }
-        else {
-            // Game continues. Now it's the opponent's turn (the turn switches from 0 to 1 and 1 to 0).
-            turnIndexAfterMove = 1 - turnIndexBeforeMove;
-            endMatchScores = null;
-        }
-        var delta = { row: row, col: col };
-        var stateAfterMove = { delta: delta, board: boardAfterMove };
-        return { endMatchScores: endMatchScores, turnIndexAfterMove: turnIndexAfterMove, stateAfterMove: stateAfterMove };
-    }
-    gameLogic.createMove = createMove;
-    function checkMoveOk(stateTransition) {
-        // We can assume that turnIndexBeforeMove and stateBeforeMove are legal, and we need
-        // to verify that the move is OK.
-        var turnIndexBeforeMove = stateTransition.turnIndexBeforeMove;
-        var stateBeforeMove = stateTransition.stateBeforeMove;
-        var move = stateTransition.move;
-        var deltaValue = stateTransition.move.stateAfterMove.delta;
-        var row = deltaValue.row;
-        var col = deltaValue.col;
-        var expectedMove = createMove(stateBeforeMove, row, col, turnIndexBeforeMove);
-        if (!angular.equals(move, expectedMove)) {
-            throw new Error("Expected move=" + angular.toJson(expectedMove, true) +
-                ", but got stateTransition=" + angular.toJson(stateTransition, true));
-        }
-    }
-    gameLogic.checkMoveOk = checkMoveOk;
-    function forSimpleTestHtml() {
-        var move = gameLogic.createMove(null, 0, 0, 0);
-        log.log("move=", move);
-        var params = {
-            turnIndexBeforeMove: 0,
-            stateBeforeMove: null,
-            move: move,
-            numberOfPlayers: 2 };
-        gameLogic.checkMoveOk(params);
-    }
-    gameLogic.forSimpleTestHtml = forSimpleTestHtml;
 })(gameLogic || (gameLogic = {}));
 //# sourceMappingURL=gameLogic.js.map
