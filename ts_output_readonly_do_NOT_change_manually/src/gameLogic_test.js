2fc415103cf7ad2b7a583c4e3d19812fb8955726
pdhar <pranav.dhar2@gmail.com>
2016-02-21 13:39:42 -0500

    added canvas drawing of nested circles

--- ts_output_readonly_do_NOT_change_manually/src/gameLogic_test.js
+++ ts_output_readonly_do_NOT_change_manually/src/gameLogic_test.js
@@ -1,31 +1,19 @@
 describe("In Carrom Game Logic", function () {
-    function expectMove(isOk, turnIndexBeforeMove, boardBeforeMove, row, col, boardAfterMove, turnIndexAfterMove, endMatchScores) {
-        var stateTransition = {
-            turnIndexBeforeMove: turnIndexBeforeMove,
-            stateBeforeMove: boardBeforeMove ? { board: boardBeforeMove, delta: null } : null,
-            move: {
-                turnIndexAfterMove: turnIndexAfterMove,
-                endMatchScores: endMatchScores,
-                stateAfterMove: { board: boardAfterMove, delta: { row: row, col: col } }
-            },
-            numberOfPlayers: null
-        };
-        if (isOk) {
-            gameLogic.checkMoveOk(stateTransition);
-        }
-        else {
-            // We expect an exception to be thrown :)
-            var didThrowException = false;
-            try {
-                gameLogic.checkMoveOk(stateTransition);
-            }
-            catch (e) {
-                didThrowException = true;
-            }
-            if (!didThrowException) {
-                throw new Error("We expect an illegal move, but checkMoveOk didn't throw any exception!");
-            }
-        }
+    function testSomething(a) {
+        if (a)
+            throw new Error("We expect an illegal move, but checkMoveOk didn't throw any exception!");
     }
+    function testSomeValue() {
+        return 1;
+    }
+    it("testing some bs function", function () {
+        testSomething(false);
+    });
+    it("testing some bs function 2", function () {
+        testSomething(false);
+    });
+    it("testing some bs function returning value", function () {
+        expect(testSomeValue()).toBe(2);
+    });
 });
 //# sourceMappingURL=gameLogic_test.js.map
