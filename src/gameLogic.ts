7653f61f23c999ff618401d77e91576753193c3a
Ismail Mustafa <ismailmustafa@rocketmail.com>
2016-03-04 11:20:47 -0500

    Refactor gameLogic. Implement initial board setup test

--- src/gameLogic.ts
+++ src/gameLogic.ts
@@ -25,8 +25,9 @@ enum CoinType {
 }
 
 interface Coin {
-    xPos: number;
-    yPos: number;
+    coordinate : Coordinate;
+    //xPos: number;
+    //yPos: number;
     size: number;
     type: CoinType;
 }
@@ -37,34 +38,33 @@ interface IState {
 
 module gameLogic {
   // Coin counts
-  export const NUMCOINS = 19;
-  export const NUMBLACK = 9;
-  export const NUMWHITE = 9;
-  export const NUMQUEEN = 1;
   
-  // Coin initializer properties
-  export const FIRSTLAYER = 6;
-  export const SECONDLAYER = 12;
-  export const COINSIZE = 1;
-  
-  // Board dimensions
-  export const BOARDWIDTH = 40;
-  export const BOARDHEIGHT = 40;
-  
-  // Starting coin layer 
-  export const NUMBEROFSIDES = 6;
-  export const HEXSIZE = COINSIZE*2;
+  let coinSize = 1;
+  export let settings : {[setting : string] : number} = {
+    "numCoins" : 19,
+    "numBlack" : 9,
+    "numWhite" : 9,
+    "numQueen" : 1,
+    "firstLayer" : 6,
+    "secondLayer" : 12,
+    "coinSize" : coinSize,
+    "boardWidth" : 40,
+    "boardHeight" : 40,
+    "numberOfSides" : 6,
+    "hexSize" : coinSize*2
+  };
 
   // Return initial board 
-  export function getInitialBoard(): Board {
-    let boardSize : BoardSize = getInitialSize();
+  export function getInitialBoard(gameSettings : {[setting : string] : number}): Board {
+    let boardSize : BoardSize = getInitialSize(gameSettings);
     let board: Board = [];
     
     // set the position of hte queen
     // insert that qqueen into board
-    let queen : Coin = {xPos:boardSize.centerX,
-                        yPos:boardSize.centerY,
-                        size:COINSIZE,
+    let queenCoordinate : Coordinate = {xPos: boardSize.centerX,
+                                        yPos: boardSize.centerY};
+    let queen : Coin = {coordinate:queenCoordinate,
+                        size:gameSettings["coinSize"],
                         type:CoinType.Queen};
 
     // Initialize two layers
@@ -74,51 +74,52 @@ module gameLogic {
     
     let color : boolean = false;
     let circles : Coordinate[] = [];
-    for (let i = 1; i <= NUMBEROFSIDES; i++) {
-      let c = getCoordinates(boardSize.centerX, boardSize.centerY, i, HEXSIZE);
-      let c2 = getCoordinates(boardSize.centerX, boardSize.centerY, i, HEXSIZE*2);
+    for (let i = 1; i <= gameSettings["numberOfSides"]; i++) {
+      let c = getCoordinates(gameSettings, boardSize.centerX, boardSize.centerY, i, gameSettings["hexSize"]);
+      let c2 = getCoordinates(gameSettings, boardSize.centerX, boardSize.centerY, i, gameSettings["hexSize"]*2);
       circles.push(c2);
       let coinType = color ? CoinType.Black : CoinType.White;
       color = !color;
-      coins.push(createCoin(c, coinType)); 
-      coins.push(createCoin(c2, coinType));
+      coins.push(createCoin(gameSettings, c, coinType)); 
+      coins.push(createCoin(gameSettings, c2, coinType));
     }
     
     color = false;
     for (let i = 0; i < circles.length; i++) {
-      let c : Coordinate = {xPos: (circles[i].xPos + circles[(i+1)%NUMBEROFSIDES].xPos)/2.0,
-                            yPos: (circles[i].yPos + circles[(i+1)%NUMBEROFSIDES].yPos)/2.0};
+      let c : Coordinate = {xPos: (circles[i].xPos + circles[(i+1)%gameSettings["numberOfSides"]].xPos)/2.0,
+                            yPos: (circles[i].yPos + circles[(i+1)%gameSettings["numberOfSides"]].yPos)/2.0};
       let coinType = color ? CoinType.Black : CoinType.White;
       color = !color;
-      coins.push(createCoin(c, coinType)); 
+      coins.push(createCoin(gameSettings, c, coinType)); 
     }
     return coins;
   }
   
-  function createCoin(c : Coordinate, coinType : CoinType) : Coin {
-    let coin : Coin = {xPos:c.xPos,
-                         yPos:c.yPos,
-                         size:COINSIZE,
+  function createCoin(gameSettings : {[setting : string] : number}, c : Coordinate, coinType : CoinType) : Coin {
+    let coinCoordinate : Coordinate = {xPos:c.xPos,
+                                   yPos:c.yPos};
+    let coin : Coin = {coordinate:coinCoordinate,
+                         size:gameSettings["coinSize"],
                          type:coinType};
     return coin;
   }
   
-  function getCoordinates(centerX : number, centerY : number, index : number, hexSize : number) : Coordinate {
+  function getCoordinates(gameSettings : {[setting : string] : number}, centerX : number, centerY : number, index : number, hexSize : number) : Coordinate {
     
-    let c : Coordinate = {xPos: centerX + hexSize * Math.cos(index * 2 * Math.PI / NUMBEROFSIDES),
-                          yPos: centerY + hexSize * Math.sin(index * 2 * Math.PI / NUMBEROFSIDES)};
+    let c : Coordinate = {xPos: centerX + hexSize * Math.cos(index * 2 * Math.PI / gameSettings["numberOfSides"]),
+                          yPos: centerY + hexSize * Math.sin(index * 2 * Math.PI / gameSettings["numberOfSides"])};
     return c;
   }
   
-  export function getInitialSize(): BoardSize {
-      let boardSize : BoardSize = {width: BOARDWIDTH, 
-                                   height: BOARDHEIGHT,
-                                   centerX: BOARDWIDTH/2,
-                                   centerY: BOARDHEIGHT/2};
+  export function getInitialSize(gameSettings : {[setting : string] : number}): BoardSize {
+      let boardSize : BoardSize = {width: gameSettings["boardWidth"], 
+                                   height: gameSettings["boardHeight"],
+                                   centerX: gameSettings["boardWidth"]/2,
+                                   centerY: gameSettings["boardHeight"]/2};
       return boardSize;
   }
 
   export function getInitialState(): IState {
-    return {board: getInitialBoard(), boardSize: getInitialSize()};
+    return {board: getInitialBoard(gameLogic.settings), boardSize: getInitialSize(gameLogic.settings)};
   }
 }
--- src/gameLogic_test.ts
+++ src/gameLogic_test.ts
@@ -1,38 +1,67 @@
 describe("In Carrom Game Logic", function() {
+  
+  let coinSize = 1;
+  let settings : {[setting : string] : number} = {
+    "numCoins" : 19,
+    "numBlack" : 9,
+    "numWhite" : 9,
+    "numQueen" : 1,
+    "firstLayer" : 6,
+    "secondLayer" : 12,
+    "coinSize" : coinSize,
+    "boardWidth" : 40,
+    "boardHeight" : 40,
+    "numberOfSides" : 6,
+    "hexSize" : coinSize*2
+  };
 
+  // Test if board creation function places all coins at correct
+  // locations on board
   function testInitialBoardState() {
-    let boardSize : BoardSize = gameLogic.getInitialSize();
-    let queen : Coin = {xPos:boardSize.centerX,
-                        yPos:boardSize.centerY,
-                        size:gameLogic.COINSIZE,
-                        type:CoinType.Queen};
-    let coins = gameLogic.getInitialBoard();
+    let coins = gameLogic.getInitialBoard(settings);
     
-    let testCoordinates : Coordinate[] = [];
-    testCoordinates.push({xPos: 0, yPos: 0});
-    testCoordinates.push({xPos: 1, yPos: 1.73205080756888});
-    testCoordinates.push({xPos: 2, yPos: 3.464101615137753});
-    testCoordinates.push({xPos: 9, yPos: 1.73205080756888});
-    testCoordinates.push({xPos: -2, yPos: 3.464101615137753});
-    testCoordinates.push({xPos: -2, yPos: 0});
-    testCoordinates.push({xPos: -4, yPos: 0});
-    testCoordinates.push({xPos: -1, yPos: -1.732051});
-    testCoordinates.push({xPos: -2, yPos: -3.4641017});
-    testCoordinates.push({xPos: 1, yPos: -1.732051});
-    testCoordinates.push({xPos: 2, yPos: -3.4641017});
-    testCoordinates.push({xPos: 2, yPos: 0});
-    testCoordinates.push({xPos: 4, yPos: 0});
-    testCoordinates.push({xPos: 0, yPos: 3.464101615137753});
-    testCoordinates.push({xPos: -3, yPos: 1.732050807568875});
-    testCoordinates.push({xPos: -3, yPos: -1.732051});
-    testCoordinates.push({xPos: 0, yPos: -3.4641017});
-    testCoordinates.push({xPos: 3, yPos: -1.732051});
-    testCoordinates.push({xPos: 3, yPos: 1.732050807568875});
+    let testCoordinates : Coordinate[] = [
+      {xPos:20, yPos:20},
+      {xPos:21, yPos:21.73205080756888},
+      {xPos:22, yPos:23.464101615137753},
+      {xPos:19, yPos:21.73205080756888},
+      {xPos:18, yPos:23.464101615137753},
+      {xPos:18, yPos:20},
+      {xPos:16, yPos:20},
+      {xPos:19, yPos:18.267949192431125},
+      {xPos:18, yPos:16.535898384862247},
+      {xPos:21, yPos:18.26794919243112},
+      {xPos:22, yPos:16.535898384862247},
+      {xPos:22, yPos:20},
+      {xPos:24, yPos:20},
+      {xPos:20, yPos:23.464101615137753},
+      {xPos:17, yPos:21.732050807568875},
+      {xPos:17, yPos:18.267949192431125},
+      {xPos:20, yPos:16.535898384862247},
+      {xPos:23, yPos:18.267949192431125},
+      {xPos:23, yPos:21.732050807568875}
+      ];
     
-    let matchCount : number = 0;
     for (let i = 0; i < coins.length; i++) {
-      let s = "circle(" + coins[i].xPos + ", " + coins[i].yPos + ",1)";
-      console.log(s);
+      let xPosValidate = compare(testCoordinates[i].xPos, coins[i].coordinate.xPos);
+      let yPosValidate = compare(testCoordinates[i].yPos, coins[i].coordinate.yPos);
+      expect(xPosValidate).toBeTruthy();
+      expect(yPosValidate).toBeTruthy();
+      if (!xPosValidate || !yPosValidate) {
+        throw new Error("\n||||||||||||||||||||||||||||||||||||||||||||||||||||||\n"
+                         + "Failed on coin number " 
+                         + i 
+                         + ":\nActual coordinate:    {" 
+                         + testCoordinates[i].xPos
+                         + ", "
+                         + testCoordinates[i].yPos
+                         + "}\nGenerated coordinate: {" 
+                         + coins[i].coordinate.xPos 
+                         + ", " 
+                         + coins[i].coordinate.yPos 
+                         + "}"
+                         + "\n||||||||||||||||||||||||||||||||||||||||||||||||||||||\n");
+      }
     }
   }
   
@@ -42,7 +71,7 @@ describe("In Carrom Game Logic", function() {
     else return false;
   }
   
-  it("should do some stuff", function() {
+  it("should ensure that all coins are placed on correct initial positions", function() {
     testInitialBoardState();
   });
   
