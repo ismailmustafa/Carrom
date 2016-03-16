2e8828a204b0e2870e01b327e80864debd37a16c
pdhar <pranav.dhar2@gmail.com>
2016-03-16 13:48:28 -0400

    added border object logic

--- src/game.ts
+++ src/game.ts
@@ -226,217 +226,87 @@ module game {
     };
   }
   
+  // export function init() {
+
+  //   // resizeGameAreaService.setWidthToHeight(1);
+  //   var heightOutput = window.innerHeight;
+  //   var widthOutput = window.innerWidth;
+      
+  //   drawBoard(widthOutput, heightOutput);
+  //   state = gameLogic.getInitialState(settings);
+  //   board = state.board;
+    
+  //   window.onresize = function(){
+  //     var heightOutput = window.innerHeight;
+  //     var widthOutput = window.innerWidth;
+      
+  //     document.getElementById("gameArea").offsetHeight;
+  //     document.getElementById("gameArea").offsetWidth;
+      
+  //     $rootScope.$apply(function () {
+  //       drawBoard(widthOutput, heightOutput);
+  //       state = gameLogic.getInitialState(settings);
+  //       board = state.board;
+  //     });
+  //   }
+  // } 
+  
   export function init() {
 
-    // resizeGameAreaService.setWidthToHeight(1);
-    var heightOutput = window.innerHeight;
-    var widthOutput = window.innerWidth;
-      
-    drawBoard(widthOutput, heightOutput);
-    state = gameLogic.getInitialState(settings);
-    board = state.board;
-    
-    window.onresize = function(){
-      var heightOutput = window.innerHeight;
-      var widthOutput = window.innerWidth;
-      
-      document.getElementById("gameArea").offsetHeight;
-      document.getElementById("gameArea").offsetWidth;
-      
-      $rootScope.$apply(function () {
-        drawBoard(widthOutput, heightOutput);
-        state = gameLogic.getInitialState(settings);
-        board = state.board;
-      });
-    }
-  } 
-  
+      var Engine = Matter.Engine,
+          World = Matter.World,
+          Bodies = Matter.Bodies;
+
+      // create a Matter.js engine
+      var engine = Engine.create(document.getElementById("gameArea"));
+
+      var c = (<any>$("#gameArea canvas")).get(0);
+
+      var _sceneWidth = c.width;
+      var _sceneHeight = c.height;
+
+
+      drawBoard(_sceneWidth, _sceneHeight);
+
+      console.log(settings["outerBoardWidth"]);
+
+      var offset = 10;
+      var _borderWidth =
+          console.log(_sceneWidth, _sceneHeight);
+
+
+
+      var widthOffset = (_sceneWidth - settings["outerBoardWidth"]) / 2.0;
+      var heightOffset = (_sceneHeight - settings["outerBoardHeight"]) / 2.0;
+
+      var topBorder = Bodies.rectangle((settings["outerBoardWidth"] / 2.0) + widthOffset,
+          (settings["borderThickness"] / 2.0) - heightOffset,
+          settings["outerBoardWidth"],
+          settings["borderThickness"],
+          { isStatic: true });
+      console.log(settings["innerBoardHeight"]);
+      var bottomBorder = Bodies.rectangle((settings["outerBoardWidth"] / 2.0) + widthOffset,
+          (settings["borderThickness"] / 2.0) - heightOffset + settings["innerBoardHeight"],
+                                settings["outerBoardWidth"],
+                                settings["borderThickness"],
+                                { isStatic: true });
+
+    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+    // add all of the bodies to the world
+    World.add(engine.world, [bottomBorder]);
+
+    // run the engine
+    Engine.run(engine);
+
+  }
+
 }
-//   // I export all variables to make it easy to debug in the browser by
-//   // simply typing in the console:
-//   // game.state
-//   export let animationEnded = false;
-//   export let canMakeMove = false;
-//   export let isComputerTurn = false;
-//   export let move: IMove = null;
-//   export let state: IState = null;
-//   export let isHelpModalShown: boolean = false;
-
-//   export function init() {
-//     translate.setTranslations(getTranslations());
-//     translate.setLanguage('en');
-//     log.log("Translation of 'RULES_OF_TICTACTOE' is " + translate('RULES_OF_TICTACTOE'));
-//     resizeGameAreaService.setWidthToHeight(1);
-//     moveService.setGame({
-//       minNumberOfPlayers: 2,
-//       maxNumberOfPlayers: 2,
-//       checkMoveOk: gameLogic.checkMoveOk,
-//       updateUI: updateUI
-//     });
-
-//     // See http://www.sitepoint.com/css3-animation-javascript-event-handlers/
-//     document.addEventListener("animationend", animationEndedCallback, false); // standard
-//     document.addEventListener("webkitAnimationEnd", animationEndedCallback, false); // WebKit
-//     document.addEventListener("oanimationend", animationEndedCallback, false); // Opera
-
-//     let w: any = window;
-//     if (w["HTMLInspector"]) {
-//       setInterval(function () {
-//         w["HTMLInspector"].inspect({
-//           excludeRules: ["unused-classes", "script-placement"],
-//         });
-//       }, 3000);
-//     }
-//   }
-
-//   function getTranslations(): Translations {
-//     return {
-//       RULES_OF_TICTACTOE: {
-//         en: "Rules of BattleTetris",
-//         iw: "חוקי המשחק",
-//       },
-//       RULES_SLIDE1: {
-//         en: "You and your opponent take turns to mark the grid in an empty spot. The first mark is X, then O, then X, then O, etc.",
-//         iw: "אתה והיריב מסמנים איקס או עיגול כל תור",
-//       },
-//       RULES_SLIDE2: {
-//         en: "The first to mark a whole row, column or diagonal wins.",
-//         iw: "הראשון שמסמן שורה, עמודה או אלכסון מנצח",
-//       },
-//       CLOSE:  {
-//         en: "Close",
-//         iw: "סגור",
-//       },
-//     };
-//   }
-
-//   function animationEndedCallback() {
-//     $rootScope.$apply(function () {
-//       log.info("Animation ended");
-//       animationEnded = true;
-//       sendComputerMove();
-//     });
-//   }
-
-//   function sendComputerMove() {
-//     if (!isComputerTurn) {
-//       return;
-//     }
-//     isComputerTurn = false; // to make sure the computer can only move once.
-//     moveService.makeMove(aiService.findComputerMove(move));
-//   }
-
-//   function updateUI(params: IUpdateUI): void {
-//     log.info("Game got updateUI:", params);
-//     animationEnded = false;
-//     move = params.move;
-//     state = move.stateAfterMove;
-//     if (!state) {
-//       state = gameLogic.getInitialState();
-//     }
-//     canMakeMove = move.turnIndexAfterMove >= 0 && // game is ongoing
-//       params.yourPlayerIndex === move.turnIndexAfterMove; // it's my turn
-
-//     // Is it the computer's turn?
-//     isComputerTurn = canMakeMove &&
-//         params.playersInfo[params.yourPlayerIndex].playerId === '';
-//     if (isComputerTurn) {
-//       // To make sure the player won't click something and send a move instead of the computer sending a move.
-//       canMakeMove = false;
-//       // We calculate the AI move only after the animation finishes,
-//       // because if we call aiService now
-//       // then the animation will be paused until the javascript finishes.
-//       if (!state.delta) {
-//         // This is the first move in the match, so
-//         // there is not going to be an animation, so
-//         // call sendComputerMove() now (can happen in ?onlyAIs mode)
-//         sendComputerMove();
-//       }
-//     }
-//   }
-
-//   export function cellClicked(row: number, col: number): void {
-//     log.info("Clicked on cell:", row, col);
-//     if (window.location.search === '?throwException') { // to test encoding a stack trace with sourcemap
-//       throw new Error("Throwing the error because URL has '?throwException'");
-//     }
-//     if (!canMakeMove) {
-//       return;
-//     }
-//     try {
-//       let nextMove = gameLogic.createMove(
-//           state, row, col, move.turnIndexAfterMove);
-//       canMakeMove = false; // to prevent making another move
-//       moveService.makeMove(nextMove);
-//     } catch (e) {
-//       log.info(["Cell is already full in position:", row, col]);
-//       return;
-//     }
-//   }
-
-//   export function shouldShowImage(row: number, col: number): boolean {
-//     let cell = state.board[row][col];
-//     return cell !== "";
-//   }
-
-//   export function isPieceX(row: number, col: number): boolean {
-//     return state.board[row][col] === 'X';
-//   }
-
-//   export function isPieceO(row: number, col: number): boolean {
-//     return state.board[row][col] === 'O';
-//   }
-
-//   export function shouldSlowlyAppear(row: number, col: number): boolean {
-//     return !animationEnded &&
-//         state.delta &&
-//         state.delta.row === row && state.delta.col === col;
-//   }
-
-//   export function clickedOnModal(evt: Event) {
-//     if (evt.target === evt.currentTarget) {
-//       evt.preventDefault();
-//       evt.stopPropagation();
-//       isHelpModalShown = false;
-//     }
-//     return true;
-//   }
-// }
-
 
 angular.module('myApp', ['ngTouch', 'ui.bootstrap', 'gameServices'])
   .run(function () {
     $rootScope['game'] = game;
-    // game.init();
-
-    // Matter.js module aliases
-    var Engine = Matter.Engine,
-      World = Matter.World,
-      Bodies = Matter.Bodies;
-
-    // create a Matter.js engine
-    var engine = Engine.create(document.getElementById("gameArea"));
-    // var parent = document.getElementById("gameArea");
-    // var canvas = parent.getElementsByTagName('canvas')[0]
-
-    var c = $("#gameArea canvas");    
-
-    var _sceneWidth = document.documentElement.clientWidth;
-    var _sceneHeight = document.documentElement.clientHeight;
-    var offset = 5;
-
-    // create two boxes and a ground
-    var boxA = Bodies.rectangle(_sceneWidth * 0.5, -offset, _sceneWidth + 0.5, 50.5, { isStatic: true });
-    var boxB = Bodies.rectangle(450, 50, 80, 80);
-    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
-    // add all of the bodies to the world
-    World.add(engine.world, [ground]);
-
-    // run the engine
-    Engine.run(engine);
     
-
-    // respondCanvas();
+    game.init();
 
   });
